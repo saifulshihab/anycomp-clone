53b57c7ccccfee9354e1995be845b34e5e1d22ab
saifulshihab <shihabmd1970@gmail.com>
2025-12-19 21:56:38 +0600

    initiate frontend codebase

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { theme } from "@/theme";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import type { Metadata } from "next";
+import { Red_Hat_Display } from "next/font/google";
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "Anycomp Clone",
+  description:
+    "Connect with a licensed Company Secretary and appoint them to register your company."
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${redHatDisplay.className}`} suppressHydrationWarning>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Alert } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+      <Alert severity="warning">Anycomp Clone Web App. Coming Soon</Alert>
+    </div>
+  );
+}
